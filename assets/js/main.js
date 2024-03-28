848641fda16a30101d69ef685c5506f266e3e504
lipcseie <lipcsei.e@gmail.com>
2024-03-28 15:49:07 +0100

    remove comments

--- assets/js/main.js
+++ assets/js/main.js
@@ -60,10 +60,11 @@ themeButton.addEventListener("click", () => {
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
 const sr = ScrollReveal({
-  origin: 'top',
-  distance: '60px',
+  origin: "top",
+  distance: "60px",
   duration: 2500,
   delay: 400,
+  reset:true,
 });
 
 sr.reveal(`.profile__border`);
