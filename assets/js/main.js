6875202936a6cac26fe3a028cfa1bbea609ac250
lipcseie <lipcsei.e@gmail.com>
2024-03-28 14:00:19 +0100

    add theme button to the eduvcation pages

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,6 +23,7 @@ tabs.forEach((tab) => {
 
 /*=============== DARK LIGHT THEME ===============*/
 const themeButton = document.getElementById("theme-button");
+
 const darkTheme = "dark-theme";
 const iconTheme = "ri-sun-line";
 
